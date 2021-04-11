3a483c7bfefac37170a47618e9378f0a1b4bb387
Roberta <56247680+RobertaUntaru@users.noreply.github.com>
2021-04-11 21:46:46 +0300

    Update WorkSite.java

--- WorkSite.java
+++ WorkSite.java
@@ -8,17 +8,21 @@ public class WorkSite{
 	public static int CarsCounter = 5; 
 	
 	public static void main(String[] args) {
+	long start = System.currentTimeMillis();
 	PCMonitor monitor = new PCMonitor(); 
 	Semaphore semaphore = new Semaphore(1); 
 	Beam BeamVector[] = new Beam[beamsNeeded]; 
 	Cars CarsVector[] = new Cars[CarsCounter];
-    for(int BeamsID = 0; BeamsID < beamsNeeded; BeamsID++){
+    	for(int BeamsID = 0; BeamsID < beamsNeeded; BeamsID++){
     	BeamVector[BeamsID] = new Beam(monitor,BeamsID); 
        	BeamVector[BeamsID].start(); 
-    }
-    for(int CarID = 0; CarID < CarsCounter; CarID++){
+    	}
+    	for(int CarID = 0; CarID < CarsCounter; CarID++){
     	CarsVector[CarID] = new Cars(monitor,CarID,semaphore); 
     	CarsVector[CarID].start(); 
-    }
-	}
+    	}
+	long end = System.currentTimeMillis();
+    	NumberFormat formatter = new DecimalFormat("#0.00000");
+    	System.out.print("Execution time is " + formatter.format((end - start) / 1000d) + " seconds");
+        }
 }
