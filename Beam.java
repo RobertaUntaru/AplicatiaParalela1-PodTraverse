36519572d787e6125c70bb1e5311e3a052d256ce
Roberta <56247680+RobertaUntaru@users.noreply.github.com>
2021-06-01 22:56:03 +0300

    Update Beam.java

--- Beam.java
+++ Beam.java
@@ -12,7 +12,6 @@ public class Beam extends Thread{
     }
     
     public void run() { 
-     System.out.println("Beam no:[" + BeamNumber +"] successfully created.");
      BeamNumber++;
      monitor.put(BeamNumber); 
     }
